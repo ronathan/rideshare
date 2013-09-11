c7a3ab58860248b8dd6c4279903d34de8676119a
ronathan <ronaldvictorino@gmail.com>
2013-09-11 22:00:39 +0000

    Restarted rideshare app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rideshare::Application.config.secret_key_base = '099934b1a75403ee1bb55733c685cb55c1a1d69b1b59c884caae50cbd193cecb372c5292026a0c34d663f968d8027e4f2b285ae39037db1ae2eedcf5e57f8bcb'
